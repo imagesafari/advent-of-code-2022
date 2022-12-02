20ee565abdc8f57c1de69b0618804c3587d0b896
imagesafari <jon@anhold.com>
2022-12-02 12:30:44 -0500

    day 2

---
+++ day2.sh
@@ -0,0 +1,1 @@
+cat input2.txt | awk '{ if ($1=="A") {a=0} ; if ($1=="B") {a=1} ; if ($1=="C") {a=2}; if ($2=="X") {b=0} ; if ($2=="Y") {b=1} ; if ($2=="Z") {b=2}; c = (3 + a - b) % 3 ; if (c==0) {d=3} ; if (c==1) {d=0} ; if (c==2) {d=6} ; e = (b+1+d) ; printf "%s %s %s %s %s\n", a, b, c, d, e }' | cut -d ' ' -f 5 | paste -s -d+ - | bc
