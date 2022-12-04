62d05818005378cf8c0da564f487e688166f0fca
imagesafari <jon@anhold.com>
2022-12-03 19:51:41 -0500

    ffs

--- day3.sh
+++ day3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # part 1
-# cheat with foo.txt mapping, working on it....
+# cheat with foo.txt mapping, i am certain there is a way to do this without writing it to a file, but...
 
 j=0;for i in {{a..z},{A..Z}}; do j=$((j+1));echo "$i:$j";done > foo.txt; cat input3.txt | awk 'BEGIN{FS=""}{for(i=1;i<=NF/2;i++)printf $i}{printf " "}{for(i=NF/2+1;i<=NF;i++){printf $i}{printf "\n"}}' | awk '{ print "tr -dc " $2 " <<< " $1 "; echo"}' | bash -e | cut -c1 | awk '{ print "grep " $1 " foo.txt"}' | bash -e | cut -d':' -f 2 | awk '{s+=$1} END {print s}' ; rm foo.txt
 
