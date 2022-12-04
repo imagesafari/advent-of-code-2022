3b1070d01b90c00258267d532d064206242dec57
imagesafari <jon@anhold.com>
2022-12-03 21:15:04 -0500

    woo figured it out

--- day3.sh
+++ day3.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # part 1
 # cheat with foo.txt mapping, i am certain there is a way to do this without writing it to a file, but...
+# WOO figured it out.
 
-j=0;for i in {{a..z},{A..Z}}; do j=$((j+1));echo "$i:$j";done > foo.txt; cat input3.txt | awk 'BEGIN{FS=""}{for(i=1;i<=NF/2;i++)printf $i}{printf " "}{for(i=NF/2+1;i<=NF;i++){printf $i}{printf "\n"}}' | awk '{ print "tr -dc " $2 " <<< " $1 "; echo"}' | bash -e | cut -c1 | awk '{ print "grep " $1 " foo.txt"}' | bash -e | cut -d':' -f 2 | awk '{s+=$1} END {print s}' ; rm foo.txt
+cat input3.txt | awk 'BEGIN{FS=""}{for(i=1;i<=NF/2;i++)printf $i}{printf " "}{for(i=NF/2+1;i<=NF;i++){printf $i}{printf "\n"}}' | awk '{ print "tr -dc " $2 " <<< " $1 "; echo"}' | bash -e | cut -c1 | awk '{ print "grep " $1 "  <<< `j=0;for i in {{a..z},{A..Z}}; do j=$((j+1));echo \"$i:$j\";done`"}' | bash -e | cut -d':' -f 2 | awk '{s+=$1} END {print s}'
 
 # part 2
 
-j=0;for i in {{a..z},{A..Z}}; do j=$((j+1));echo "$i:$j";done > foo.txt;cat input3.txt | paste -d " " - - - | awk '{"tr -dc " $2 " <<< " $1 | getline var1 ; "fold -w1 <<<" var1 "| sort -u | tr -d \\\\n" | getline var4;"tr -dc " $3 " <<< " $2 | getline var2; "fold -w1 <<<" var2 "|sort -u | tr -d \\\\n" | getline var5;"tr -dc " var5 " <<< " var4 | getline output ; print output}' | awk '{ print "grep " $1 " foo.txt"}' | bash -e | cut -d':' -f 2 | awk '{s+=$1} END { print s}';rm foo.txt
+cat input3.txt | paste -d " " - - - | awk '{"tr -dc " $2 " <<< " $1 | getline var1 ; "fold -w1 <<<" var1 "| sort -u | tr -d \\\\n" | getline var4;"tr -dc " $3 " <<< " $2 | getline var2; "fold -w1 <<<" var2 "|sort -u | tr -d \\\\n" | getline var5;"tr -dc " var5 " <<< " var4 | getline output ; print output}' | awk '{ print "grep " $1 " <<< `j=0;for i in {{a..z},{A..Z}}; do j=$((j+1));echo \"$i:$j\";done`"}' | bash -e | cut -d':' -f 2 | awk '{s+=$1} END { print s}'
 
 
