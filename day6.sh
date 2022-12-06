eb29093e6abe2ec5822975f8a9985c3dab7b3436
imagesafari <jon@anhold.com>
2022-12-06 02:17:58 -0500

    comments

--- day6.sh
+++ day6.sh
@@ -1,3 +1,4 @@
+# this one was fun
 # part 1
 cat input6.txt | awk '{ for (i=1;i<=length($0);i++) { block = substr($0,i,4); offset = i+3 ; printf("block: %s offset: %s ",block,offset);delete a;for (j=1;j<=length(block);j++) { foo = a[substr(block,j,1)]++; printf("%s",foo);sync[j]=foo}check = ""; for (z=1;z<=length(sync);z++) { check = check "" sync[z] };printf(" %s sync: %s",foo,check);if (check=="0000") { printf(" %s\n","detected at offset " offset);break} else { print "" }}}' | grep detected
 
